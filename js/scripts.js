1410a912ec1535ab72b7ce2833618dd03dff382e
Dziarnek <32908239+Dziarnek@users.noreply.github.com>
2017-11-04 02:48:28 +0100

    Changing 'i += +1' into 'i++'

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@ var list = document.getElementById("list"),
     i = 0;
 
 addElem.addEventListener('click', function() {
-    i += + 1;
+    i++;
     var element = document.createElement('li');
     element.innerHTML = ('Element ' + i + " liczone innym sposobem " + document.getElementsByTagName('li').length);
     list.appendChild(element);
